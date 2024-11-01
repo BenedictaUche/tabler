0922443f85476d019078201cbc5511e461f2e2df
BenedictaUche <benedictaonyebuchi.uche@gmail.com>
2024-11-01 18:49:27 +0100

    changed api fetch method

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import StatsCard from '../components/StatsCard';
+import { fetchDashboardData } from '../services/api';
 
 const Home = () => {
   const {isLoading, isError, data, error} = useQuery({
     queryKey: ['summary'],
-    queryFn: async () => {
-      const response = await axios.get('/dashboardData.json');
-      return response.data.summary;
-    }
+    queryFn: fetchDashboardData,
+    // queryFn: async () => {
+    //   const response = await axios.get('/dashboardData.json');
+    //   return response.data.summary;
+    // }
   })
 
   if(isLoading) {
@@ -24,12 +26,13 @@ const Home = () => {
     <div className="space-y-4 px-14">
       <h1 className="text-3xl font-semibold text-gray-500">Dashboard</h1>
       <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
-        {data.map((stat, index) => (
+        {data.summary.map((stat, index) => (
           <StatsCard key={index} stat={stat} />
         ))}
       </div>
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
